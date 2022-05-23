ed92d186fa3bd8a0b33769bdbb90219b3a58d692
Perry Jorss <pjorss123@gmail.com>
2022-05-23 19:56:35 -0400

    commit tree 79ee34417903553750fe77c669ac056d6a522b37

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^\w,`' -]|_/g, '');
+
+  }
+  static titleize(string){
+    return string.toLowerCase().split(" ").map(function(word,index) {
+      if(!['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(word) || index === 0){
+      return (word.charAt(0).toUpperCase() + word.slice(1))
+      }
+      else {
+        return word
+      }      
+    }).join(' ')
+  }
 }
